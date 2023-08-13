849f749e13e2457a6d587724d3aa54308d84624c
Carlos Gaubert <carlos.gaubert1801@alumnos.ubiobio.cl>
2023-08-13 18:49:55 -0400

    agregando mapa sentinel

--- mapas/views.py
+++ mapas/views.py
@@ -17,27 +17,10 @@ def maps(request):
     geometry = ee.Geometry.Polygon(
         [geometria], None, False);
 
-  
 
+    print(request.POST.get('tipo_imagen'))
 
-    # Filtrar la colección de imágenes Landsat 8
-    IMGLandsat8 = ee.ImageCollection('LANDSAT/LC08/C02/T1_RT_TOA') \
-        .filterDate('2018-04-01', '2018-12-30') \
-        .filterBounds(geometry) \
-        .filterMetadata('CLOUD_COVER', 'less_than', 20)
-
-    # Obtener la imagen mediana
-    Landsat8Filtro = IMGLandsat8.median()
-
-    # Recortar la imagen con la geometría
-    Landsat8Clip = Landsat8Filtro.clip(geometry)
-
-
-    imagenRGB = Landsat8Clip.visualize(**{'min': 0,'max': 0.5, 'bands': ['B4', 'B3', 'B2']})
-    extension = 'jpg'
-
-    url = imagenRGB.getThumbURL({ 'region': geometry, 'dimensions': 500, 'format': extension })
-    print(url)
+    
 
     form = DescargaImagenForm()
     return render(
@@ -53,4 +36,46 @@ def maps(request):
         {'form': form}
       )
 
- 
+def descargar_imagen_landsat(geometry):
+  # Filtrar la colección de imágenes Landsat 8
+  IMGLandsat8 = ee.ImageCollection('LANDSAT/LC08/C02/T1_RT_TOA') \
+      .filterDate('2018-04-01', '2018-12-30') \
+      .filterBounds(geometry) \
+      .filterMetadata('CLOUD_COVER', 'less_than', 20)
+
+  # Obtener la imagen mediana
+  Landsat8Filtro = IMGLandsat8.median()
+
+  # Recortar la imagen con la geometría
+  Landsat8Clip = Landsat8Filtro.clip(geometry)
+
+
+  imagenRGB = Landsat8Clip.visualize(**{'min': 0,'max': 0.5, 'bands': ['B4', 'B3', 'B2']})
+  extension = 'jpg'
+
+  url = imagenRGB.getThumbURL({ 'region': geometry, 'dimensions': 500, 'format': extension })
+  print(url)
+
+
+def descargar_imagen_sentinel(geometry):
+  # Load Sentinel-2 TOA reflectance data
+  dataset = ee.ImageCollection('COPERNICUS/S2') \
+      .filterDate('2021-01-01', '2021-01-31') \
+      .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20)) \
+      .filterBounds(geometry) \
+      .median()
+
+  # Visualization parameters
+  rgbVis = {
+      'min': 0.0,
+      'max': 2500,
+      'gamma': 1.1,
+      'bands': ['B4', 'B3', 'B2']
+  }
+
+  datasetClip = dataset.clip(geometry)
+  imagenRGB = datasetClip.visualize(**{'min': 0,'max': 0.5, 'bands': ['B4', 'B3', 'B2']})
+  extension = 'jpg'
+
+  url = imagenRGB.getThumbURL({ 'region': geometry, 'dimensions': 500, 'format': extension })
+  print(url)
