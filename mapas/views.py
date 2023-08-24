5bc2b6bb18d0e38f2481dda403ad6bb93530296c
CarlosGaubertQ <carlos.gaubert@sansano.usm.cl>
2023-08-24 15:58:03 -0400

    combinacion de bandas espectrales y satelite landsat 7

--- mapas/views.py
+++ mapas/views.py
@@ -1,12 +1,10 @@
 
 from tkinter import Label
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from mapas.forms import DescargaImagenForm
 from .models import Satelite, Tipo_Imagen
-import ee
 import json
-
-from matplotlib.image import imread
+import ee
 # Create your views here.
 def maps(request):
   
@@ -35,26 +33,19 @@ def maps(request):
         {'form': form}
       )
     else: 
-      geometria = json.loads(request.POST.get('geometria'))
-      # Inicializar la API de Google Earth Engine
-      ee.Initialize()
-
-      # Definir la geometría
-      geometry = ee.Geometry.Polygon(
-        [geometria], None, False);
-
 
       satelite = Satelite.objects.get(id=request.POST.get('satelite'))
       tipoImagen = Tipo_Imagen.objects.get(id=request.POST.get('tipoImagen'))
       fecha_inicio = request.POST.get('fecha_inicio')
       fecha_fin = request.POST.get('fecha_fin')
-      
 
+      
       if satelite.name == 'Landsat8':
-        url = descargar_imagen_landsat(geometry, fecha_inicio, fecha_fin)
-        
+        url = descargar_imagen_landsat8(json.loads(request.POST.get('geometria')), fecha_inicio, fecha_fin, tipoImagen)
+      elif satelite.name == 'Landsat7':
+        url = descargar_imagen_landsat7(json.loads(request.POST.get('geometria')), fecha_inicio, fecha_fin, tipoImagen)
       elif satelite.name == 'Sentinel-2':
-        url = descargar_imagen_sentinel(geometry, fecha_inicio, fecha_fin)
+        url = descargar_imagen_sentinel(json.loads(request.POST.get('geometria')), fecha_inicio, fecha_fin, tipoImagen)
       else:
         print("no existe este satelite")
 
@@ -85,9 +76,20 @@ def vista_satelite(request, url):
         {'url': url}
   )
 
-def descargar_imagen_landsat(geometry, fecha_inicio, fecha_fin):
-  # Filtrar la colección de imágenes Landsat 8
 
+def descargar_imagen_landsat8(geometry, fecha_inicio, fecha_fin, tipoImagen):
+  band = ['B4', 'B3', 'B2']
+
+  if tipoImagen.name == 'True color':
+    band = ['B4', 'B3', 'B2']
+  elif tipoImagen.name == 'False color':
+    band = ['B10', 'B4', 'B3']
+  # Inicializar la API de Google Earth Engine
+  ee.Initialize()
+
+  # Definir la geometría
+  geometry = ee.Geometry.Polygon(
+    [geometry], None, False);
   
   IMGLandsat8 = ee.ImageCollection('LANDSAT/LC08/C02/T1_RT_TOA') \
       .filterDate(fecha_inicio, fecha_fin) \
@@ -101,18 +103,67 @@ def descargar_imagen_landsat(geometry, fecha_inicio, fecha_fin):
   Landsat8Clip = Landsat8Filtro.clip(geometry)
 
 
-  imagenRGB = Landsat8Clip.visualize(**{'min': 0,'max': 0.5, 'bands': ['B4', 'B3', 'B2']})
-  extension = 'png'
+
+  imagenRGB = Landsat8Clip.visualize(**{'min': 0,'max': 0.5, 'bands': band})
+  extension = 'jpg'
 
   url = imagenRGB.getThumbURL({ 'region': geometry, 'dimensions': 500, 'format': extension })
   print(url)
   return url
   
+def descargar_imagen_landsat7(geometry, fecha_inicio, fecha_fin, tipoImagen):
+  band = ['B3', 'B2', 'B1']
+
+  if tipoImagen.name == 'True color':
+    band = ['B3', 'B2', 'B1']
+  elif tipoImagen.name == 'False color':
+    band = ['B4', 'B3', 'B2']
+
+  # Inicializar la API de Google Earth Engine
+  ee.Initialize()
+
+  # Definir la geometría
+  geometry = ee.Geometry.Polygon(
+    [geometry], None, False);
+  
+  # Filtra la colección de imágenes Landsat 7
+  l7 = (ee.ImageCollection("LANDSAT/LE07/C01/T1_TOA")
+        .filterDate(fecha_inicio, fecha_fin)
+        .filterBounds(geometry)
+        .sort('CLOUD_COVER', True)
+        .first())
+
+  # Calcula el promedio focal
+  img_fill = l7.focal_mean(1, 'square', 'pixels', 8)
+
+  # Combina la imagen promedio con la imagen Landsat 7 original
+  final_image = img_fill.blend(l7)
+
+  # Obtiene una URL para la imagen en formato JPG
+  url = final_image.clip(geometry).getThumbUrl({'min': 0, 'max': 0.3, 'gamma': 1.4, 'bands': band, 'format': 'jpg'})
+
+  print("URL de la imagen en formato JPG:", url)
+  return url
   
 
 
 
-def descargar_imagen_sentinel(geometry, fecha_inicio, fecha_fin):
+def descargar_imagen_sentinel(geometry, fecha_inicio, fecha_fin, tipoImagen):
+  
+  band = ['B4', 'B3', 'B2']
+
+  if tipoImagen.name == 'True color':
+    band = ['B4', 'B3', 'B2']
+  elif tipoImagen.name == 'False color':
+    band = ['B8', 'B4', 'B3']
+
+  
+  # Inicializar la API de Google Earth Engine
+  ee.Initialize()
+
+  # Definir la geometría
+  geometry = ee.Geometry.Polygon(
+    [geometry], None, False);
   # Load Sentinel-2 TOA reflectance data
   dataset = ee.ImageCollection('COPERNICUS/S2') \
       .filterDate(fecha_inicio, fecha_fin) \
@@ -123,7 +174,7 @@ def descargar_imagen_sentinel(geometry, fecha_inicio, fecha_fin):
 
 
   datasetClip = dataset.clip(geometry)
-  imagenRGB = datasetClip.visualize(**{'min': 0,'max': 2500, 'bands': ['B4', 'B3', 'B2']})
+  imagenRGB = datasetClip.visualize(**{'min': 0,'max': 2500, 'bands': band})
   extension = 'jpg'
 
   url = imagenRGB.getThumbURL({ 'region': geometry, 'dimensions': 500, 'format': extension })
--- prueba.py
+++ prueba.py
@@ -1,14 +1,32 @@
-import matplotlib.pyplot as plt
-import urllib.request
-from matplotlib.image import imread
+import ee
 
-# URL de la imagen que deseas visualizar
-image_url = "https://www.smashbros.com/images/og/sonic.jpg"
+# Inicializa el entorno Earth Engine
+ee.Initialize()
 
-# Descargar la imagen desde la URL
-image = imread(urllib.request.urlopen(image_url), format='jpg')
+# Define la región de interés (aoi)
+aoi = ee.Geometry.Polygon(
+        [[[-120.23666381835938, 38.2068653630926],
+          [-120.23666381835938, 36.86048636181168],
+          [-118.06060791015625, 36.86048636181168],
+          [-118.06060791015625, 38.2068653630926]]])
 
-# Mostrar la imagen utilizando Matplotlib
-plt.imshow(image)
-plt.axis('off')  # Ocultar los ejes
-plt.show()
+# Filtra la colección de imágenes Landsat 7
+l7 = (ee.ImageCollection("LANDSAT/LE07/C01/T1_TOA")
+      .filterDate('2004-01-01', '2004-12-31')
+      .filterBounds(aoi)
+      .sort('CLOUD_COVER', True)
+      .first())
+
+# Calcula el promedio focal
+img_fill = l7.focal_mean(1, 'square', 'pixels', 8)
+
+# Combina la imagen promedio con la imagen Landsat 7 original
+final_image = img_fill.blend(l7)
+
+# Define el estilo para la visualización
+l7_style = {"bands": ["B3", "B2", "B1"], "min": 0.0265324916690588, "max": 0.256043404340744, "gamma": 1}
+
+# Obtiene una URL para la imagen en formato JPG
+image_url = final_image.clip(aoi).getThumbUrl({'min': 0, 'max': 0.3, 'gamma': 1.4, 'bands': 'B3,B2,B1', 'format': 'jpg'})
+
+print("URL de la imagen en formato JPG:", image_url)
