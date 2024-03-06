52afe7eeb17b8aee95a232b102e5d91b14951476
CarlosGaubertQ <carlosgaubertquijada@gmail.com>
2024-03-06 14:04:36 -0300

    resultados finales

--- ShapeFile.py
+++ ShapeFile.py
@@ -2,13 +2,20 @@ import geopandas as gpd
 from shapely.geometry import Polygon
 
 # Crea una geometría (en este caso, un polígono)
-polygon = Polygon([[-70.87614369062425,-33.367203239240126],[-70.40648291112873,-33.36158950122372],[-70.39989651400553,-33.59156471876422],[-70.86955729350106,-33.595300959866876],[-70.87614369062425,-33.367203239240126]])
+polygon = Polygon([
+    [-72.92011097902237, -36.78100537892693],
+    [-72.92919956617702, -36.78091317492149],
+    [-72.92946747707144, -36.78896084678219],
+    [-72.91940786501739, -36.78889860913151],
+    [-72.91982435300024, -36.78582969857882],
+    [-72.92011097902237, -36.78100537892693]
+])
 
 # Crea un GeoDataFrame con la geometría
 gdf = gpd.GeoDataFrame({'geometry': [polygon]})
 
 # Define la ruta donde deseas guardar el archivo shapefile
-ruta_guardar = 'shapefilesForest/nombre_shapefile.shp'
+ruta_guardar = 'shapefile3/nombre_shapefile.shp'
 
 # Guarda el GeoDataFrame como un archivo shapefile
 gdf.to_file(ruta_guardar)
